0a84890759118a08a06fc2cf70306f88546e5e1e
Isradin <isradin.nurbek@gmail.com>
2021-08-08 09:17:12 +0300

    Create menu & modal

--- js/script.js
+++ js/script.js
@@ -54,4 +54,120 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     timer('#timer', '2021-08-29');
+
+    // Menu
+
+    const toggleMenu = (triggerSelector, menuSelector, closeBtnSelector, menuItemSelector) => {
+        const trigger = document.querySelector(triggerSelector),
+            menu = document.querySelector(menuSelector),
+            closeBtn = document.querySelector(closeBtnSelector),
+            menuItems = document.querySelectorAll(menuItemSelector);
+
+        const handlerMenu = () => {
+            menu.classList.toggle('active-menu');
+        };
+
+        trigger.addEventListener('click', handlerMenu);
+        closeBtn.addEventListener('click', handlerMenu);
+
+
+        menuItems.forEach(item => item.addEventListener('click', handlerMenu));
+
+    };
+
+    toggleMenu('.menu', 'menu', '.close-btn', 'ul>li');
+
+    //Modals
+    const modals = (triggerBtnSelector, modalSelector, closeBtnSelector) => {
+        const triggerBtn = document.querySelectorAll(triggerBtnSelector),
+            modal = document.querySelector(modalSelector),
+            closeBtn = document.querySelector(closeBtnSelector);
+
+        const openModal = () => {
+            modal.style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        };
+
+        function animate({
+            timing,
+            draw,
+            duration
+        }) {
+            const start = performance.now();
+
+            requestAnimationFrame(function animate(time) {
+                // timeFraction изменяется от 0 до 1
+                let timeFraction = (time - start) / duration;
+                if (timeFraction > 1) {
+                    timeFraction = 1;
+                }
+
+                // вычисление текущего состояния анимации
+                const progress = timing(timeFraction);
+
+                draw(progress); // отрисовать её
+
+                if (timeFraction < 1) {
+                    requestAnimationFrame(animate);
+                }
+            });
+        }
+
+        triggerBtn.forEach(trigger => {
+            trigger.addEventListener('click', e => {
+                if (e.target) {
+                    e.preventDefault();
+                }
+
+                openModal();
+
+                if (window.innerWidth > 768) {
+                    animate({
+                        duration: 500,
+                        timing(timeFraction) {
+                            return timeFraction;
+                        },
+                        draw(progress) {
+                            modal.style.opacity = progress;
+                        }
+                    });
+                    modal.style.visibility = 'visible';
+                }
+            });
+        });
+
+        const closeModal = () => {
+            modal.style.display = 'none';
+            document.body.style.overflow = '';
+        };
+
+        closeBtn.addEventListener('click', () => {
+            document.body.style.overflow = '';
+            if (window.innerWidth > 768) {
+                window.setTimeout(() => modal.style.visibility = 'hidden', 500);
+                animate({
+                    duration: 500,
+                    timing(timeFraction) {
+                        return timeFraction;
+                    },
+                    draw(progress) {
+                        modal.style.opacity = 1 - progress;
+                    }
+                });
+
+            } else {
+                closeModal();
+            }
+        });
+
+        //При клике на подложку - исчезаeт.
+        modal.addEventListener('click', e => {
+            if (e.target === modal) {
+                closeModal();
+            }
+        });
+    };
+
+    modals('.popup-btn', '.popup', '.popup-close');
+
 });
